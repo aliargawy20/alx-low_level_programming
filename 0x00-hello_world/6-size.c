1ff49cd5e640ef88c1322a25416a9efc23feba6a
aliargawy20 <ali.argawy20@gmail.com>
2023-09-07 10:12:11 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
 	printf("size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
 	printf("size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
 }
 
