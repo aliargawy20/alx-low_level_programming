f271e48606fda626524d0194d8c47d1997eb7f8f
aliargawy20 <ali.argawy20@gmail.com>
2023-09-12 08:15:53 -0700

    lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  *
  * Return: always (0);
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a' ; c <= 'z' ++c)
+	for (c = 'a' ; c <= 'z' ; c++)
 		_putchar(c);
 	_putchar('\n');
 
