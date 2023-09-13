a0a5720151473eee8931500033cc9e890625fb5b
aliargawy20 <ali.argawy20@gmail.com>
2023-09-13 10:32:55 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints the first 50 fibonacci numbers
+ *              starting with 1 and 2 folled by a new line
+ * Return (0)
+*/
+int main(void)
+{
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	unsigned long int sum;
+	int i;
+
+	printf("%lu, ", num1);
+	for (i = 1 ; i < 50 ; i++)
+	{
+		printf("%lu", num2);
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+		if (i != 49)
+			printf(", ");
+
+	}
+	printf("\n");
+	return (0);
+
+}
