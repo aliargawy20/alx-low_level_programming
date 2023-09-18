e3c33feef622432d94668f3e1f05acee217bf939
aliargawy20 <ali.argawy20@gmail.com>
2023-09-18 05:28:54 -0700

    count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - find the lenght of strings
+ * @s: string to be counted
+ * Return: length of the string
+*/
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\n' ; i++)
+		count++;
+	return (count);
+
+
+
+}
