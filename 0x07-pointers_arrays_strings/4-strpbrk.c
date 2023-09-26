803cc1376f34f367d29a44ebe009c2fe4ea565e4
aliargawy20 <ali.argawy20@gmail.com>
2023-09-26 04:01:47 -0700

    pbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - locates character in a string
+ * @s: string to search
+ * @accept: character to search in s
+ * Return: accept or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+
+
+}
