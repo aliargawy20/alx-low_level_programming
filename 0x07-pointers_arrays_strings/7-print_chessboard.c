531ab752152d1df7a2445c5d21616f492830f750
aliargawy20 <ali.argawy20@gmail.com>
2023-09-26 04:51:54 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,15 @@
   */
 void print_chessboard(char (*a)[8])
 {
-	int i, c;
+	int b, c;
 
-	for (i = 0; i < 8; i++)
+	for (b = 0; b < 8; b++)
 	{
 		for (c = 0; c < 8; c++)
-		{
-			_putchar(a[i][c]);
-		}
 
-		_putchar('\n');
+			putchar(a[b][c]);
+
+
+		putchar('\n');
 	}
 }
