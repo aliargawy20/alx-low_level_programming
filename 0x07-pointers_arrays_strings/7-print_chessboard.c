db80a3edded816e9042a766c31736df6a3bc0151
aliargawy20 <ali.argawy20@gmail.com>
2023-09-26 04:36:55 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_chessboard - Print the chessboard
+  * @a: array of pieces
+  *
+  * Return: Nothing.
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i, c;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[i][c]);
+		}
+
+		_putchar('\n');
+	}
+}
