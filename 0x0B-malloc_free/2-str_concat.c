01958741cf4418a59715f4e4487bf9df493965cc
aliargawy20 <ali.argawy20@gmail.com>
2023-10-04 04:20:16 -0700

    copy two str

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+  * str_concat - Concatenates two strings of any size
+  * @s1: the first string to concatenate
+  * @s2: the second string to concatenate
+  *
+  * Return: the two strings concatenated
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *duplicate1;
+	char *duplicate2;
+
+	if (s1 == 0 && s2 == 0)
+		return (NULL);
+	duplicate1 = malloc(strlen(s1) + 1);
+	duplicate2 = malloc(strlen(s2) + 1);
+	if (duplicate1 == 0 && duplicate2 == 0)
+		return (NULL);
+	strcpy(duplicate1, s1);
+	strcpy(duplicate2, s2);
+	return (duplicate1);
+	return (duplicate2);
+
+}
